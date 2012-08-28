827da8d280bd4d382311856f5c2d9dc2c4fdb491
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-08-27 22:13:46 -0400

    Updated code style

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 public class App
 {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
 }
